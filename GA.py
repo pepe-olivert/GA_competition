6c508e48353b29b0ffd0eb3d2d319a41279b4156
Pepe <jolisert@gmail.com>
2023-12-14 17:25:48 +0100

    Fitness function added

--- GA.py
+++ GA.py
@@ -1,9 +1,25 @@
+import numpy as np
+
 
 class GA:
+    def __init__(self,time_deadline,problem_path,**kwargs):
+        """
+        Inicializador de los objetos de la clase. Usar
+        este método para hacer todo el trabajo previo y necesario
+        para configurar el algoritmo genético
+        Args:
+            problem_path: Cadena de texto que determina la ruta en la que se encuentra la definición del problema
+            time_deadline: Límite de tiempo que el algoritmo genético puede computar
+        """
+        self.problem_path = problem_path
+        self.best_solution = None 
+        self.time_deadline = time_deadline 
+        #TODO: Completar método para configurar el algoritmo genético (e.g., seleccionar cruce, mutación, etc.)
+        
 
-    # Module to read the instances
-    def readInstances(path):
-        with open(path, "r") as f:
+    def read_problem_instance(self,problem_path):
+        
+        with open(problem_path, "r") as f:
             text = f.read()
         lines = text.strip().split('\n')
         num_locations = int(lines[0].split()[1])
@@ -11,14 +27,8 @@ class GA:
 
         matrix_lines = [line.split() for line in lines[3:]]
         distance_matrix = np.array(matrix_lines, dtype=int)
-        
 
-    def read_problem_instance(self,problem_path):
-        """
-        TODO: Implementar método para leer una instancia del problema
-        y ajustar los atributos internos del objeto necesarios
-        """
-        pass
+        return num_locations,num_vehicles,distance_matrix
 
     def get_best_solution(self):
         """
@@ -38,21 +48,15 @@ class GA:
         self.read_problem_instance(self.problem_path)
         pass
 
-    def __init__(self,time_deadline,problem_path,**kwargs):
+    def fitness(self,solution,dist_matrix):
         """
-        Inicializador de los objetos de la clase. Usar
-        este método para hacer todo el trabajo previo y necesario
-        para configurar el algoritmo genético
-        Args:
-            problem_path: Cadena de texto que determina la ruta en la que se encuentra la definición del problema
-            time_deadline: Límite de tiempo que el algoritmo genético puede computar
+        If the solution is in format : [[0,2,3,4,0][0,5,6,7,0]] We should calculate the fitness of that solution
+        as 1/total distance traveled by that salesman
         """
-        self.problem_path = problem_path
-        self.best_solution = None #Atributo para guardar la mejor solución encontrada
-        self.time_deadline = time_deadline # Límite de tiempo (en segundos) para el cómputo del algoritmo genético
-        #TODO: Completar método para configurar el algoritmo genético (e.g., seleccionar cruce, mutación, etc.)
-
-
-
-    
-
+        total_distance = 0
+        for salesman in solution:
+            for i in range(len(salesman) - 1):
+                origin = salesman[i]
+                destination = salesman[i + 1]
+                total_distance += dist_matrix[origin][destination]
+        return 1 / total_distance
